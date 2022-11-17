3eae3651aadd6a4020f72e9c2f8d378a337c0c4c
kght6123 <i.feeling.song+github@gmail.com>
2022-11-18 01:57:39 +0900

    JSXメモ追加

--- components/StepperJSX.vue
+++ components/StepperJSX.vue
@@ -24,3 +24,39 @@ export default defineComponent({
   },
 })
 </script>
+
+<!-- TODO: 下記のようなscript setup的な書き方はできない！！JSXの場合はdefineComponentのsetupまたはrenderで書く必要がある。
+  <script lang="tsx" setup>
+const props = defineProps<{
+  max: number
+}>()
+const counter = reactive<{ count: number }>({ count: 0 })
+
+defineExpose({
+  props,
+  counter,
+})
+</script>
+
+<script lang="tsx">
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  render() {
+    return (
+      <div>
+        <span>{counter.count}</span>
+        <button
+          onClick={() => {
+            counter.count <= props.max ? counter.count++ : counter.count
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    )
+  },
+})
+</script>
+
+-->
